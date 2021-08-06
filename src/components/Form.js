a6446b775b2b98069bdfa1bb282492b8e10df2f2
kino <kinonote13@gmail.com>
2021-08-06 18:07:50 +0900

    axiosをインストールして、 Formファイルにapiを実装した

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,20 @@
 import { useState } from "react";
+import axios from "axios";
+
 
 const Form =()=>{
   const [city,setCity]=useState("");
+  const getWeather=(e)=>{
+    e.preventDefault();
+    axios.get("http://api.weatherapi.com/v1/current.json?key=0ea0bc9c09b54d5984883022210608&q=London&aqi=no")
+    .then(res=>console.log(res))
+  }
 
   return(
     <form>
       <input type="text" name="city" placeholder="都市名" 
       onChange={e=>setCity(e.target.value)}/>
-      {city}
-      <button type="submit">Get Weather</button>
+      <button type="submit" onClick={getWeather}>Get Weather</button>
     </form>
   );
 };
