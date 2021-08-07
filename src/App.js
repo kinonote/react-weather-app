b38912ca3e688763d4cc68fa67dcf356b7f737a7
kino <kinonote13@gmail.com>
2021-08-07 17:16:14 +0900

    FormファイルのデータをAppファイルに移した その際、propsを実装した

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { useState } from "react";
+import axios from "axios";
 import Title from './components/Title';
 import Form from './components/Form';
+import Results from './components/Results';
 import './App.css';
 
 function App() {
+  const [city,setCity]=useState("");
+  const getWeather=(e)=>{
+    e.preventDefault();
+    axios.get("http://api.weatherapi.com/v1/current.json?key=0ea0bc9c09b54d5984883022210608&q=London&aqi=no")
+    .then(res=>console.log(res))
+  }
+
   return (
     <div className="App">
-      <Title />
-      <Form />
+      <Title /> 
+      <Form setCity={setCity} getWeather={getWeather} />
+      <Results />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,11 @@
-import { useState } from "react";
-import axios from "axios";
 
-
-const Form =()=>{
-  const [city,setCity]=useState("");
-  const getWeather=(e)=>{
-    e.preventDefault();
-    axios.get("http://api.weatherapi.com/v1/current.json?key=0ea0bc9c09b54d5984883022210608&q=London&aqi=no")
-    .then(res=>console.log(res))
-  }
+const Form =(props)=>{
 
   return(
     <form>
       <input type="text" name="city" placeholder="都市名" 
-      onChange={e=>setCity(e.target.value)}/>
-      <button type="submit" onClick={getWeather}>Get Weather</button>
+      onChange={e=>props.setCity(e.target.value)}/>
+      <button type="submit" onClick={props.getWeather}>Get Weather</button>
     </form>
   );
 };
