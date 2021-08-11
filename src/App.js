9c34209baeb14ebf779c07ac639216b737ed6dbb
kino <kinonote13@gmail.com>
2021-08-11 21:44:30 +0900

    catch構文を実装した

--- src/App.js
+++ src/App.js
@@ -25,14 +25,16 @@ function App() {
         conditionText:res.data.current.condition.text,
         icon:res.data.current.condition.icon
       })
+      setCity("");
     })
+    .catch(err=>alert("都市名を入力するところです。都市名は英語でお願いします。ページをリロードして、もう一度試してください。"))
   }
 
   return (
     <div className="wrapper">
       <div className="App">
         <Title /> 
-        <Form setCity={setCity} getWeather={getWeather} />
+        <Form setCity={setCity} getWeather={getWeather} city={city} />
         <Results results={results} />
       </div>
     </div>
