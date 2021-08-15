5040ff46b4e10960aa370fb0e2612ddf8c239cbe
kino <kinonote13@gmail.com>
2021-08-15 13:05:03 +0900

    タイトルを変えた

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ const Form =({city,setCity,getWeather})=>{
 
   return(
     <form onSubmit={getWeather}>
-      <input type="text" name="city" placeholder="都市名" 
+      <input type="text" name="city" placeholder="都市名(English)" 
       onChange={e=>setCity(e.target.value)} value={city}/>
       <button type="submit">Get Weather</button>
     </form>
